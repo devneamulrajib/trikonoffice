968e8b2739b941ea84fe9f287f1b4295a89607b1
Rajib <dev.neamulrajib@gmail.com>
2026-06-25 13:27:33 +0600

    ko

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-// This points to the "dist" folder (where Vite builds your files)
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Redirect all traffic to index.html so React Router works
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
